8ba7c80b384c073d417c96280a3e64ca8ad10881
Tristan <tristan.heckelsmueller@cursade.de>
2017-12-18 17:41:04 +0100

    added calendar js, added nav_bar.js, few css index correcting

---
+++ app/assets/scripts/nav_bar.js
@@ -0,0 +1,6 @@
+console.log("Nav_Bar")
+$( window ).scroll(function() {
+  $(".site-header__logo").css("padding", "0em");
+  $(".site-header__logo").css("background", "unset");
+  console.log("Nav_Bar")
+});
